9c8d2f998a209fed9c97e13b9bc3c380475fca96
Shishir Gopinath <11451052+hvsg@users.noreply.github.com>
2026-09-04 16:12:57 +0000

    Restore frame pacing for euroc

--- Examples/Stereo-Inertial/stereo_inertial_euroc.cc
+++ Examples/Stereo-Inertial/stereo_inertial_euroc.cc
@@ -322,8 +322,8 @@ int main(int argc, char **argv)
             else if(ni>0)
                 T = tframe-vTimestampsCam[seq][ni-1];
 
-            // if(ttrack<T)
-            //     usleep((T-ttrack)*1e6); // 1e6
+            if(ttrack<T)
+                usleep((T-ttrack)*1e6); // 1e6
         }
 
         if(seq < num_seq - 1)
